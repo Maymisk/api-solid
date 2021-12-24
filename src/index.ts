47ea00bb57950e0ca1be5df08d1e50ccd03597fa
Khalil M. M. Bohner <khalilbohner@gmail.com>
2021-12-24 01:30:53 -0300

    added documentation - route /api-docs

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,12 @@ import express from "express";
 
 import { usersRoutes } from "./routes/users.routes";
 
+import swaggerUi from 'swagger-ui-express'
+import swaggerFile from '../swagger..json'
+
 const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(express.json());
 
